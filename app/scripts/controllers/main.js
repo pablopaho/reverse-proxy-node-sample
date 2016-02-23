011a6e543405ca11d311d1f656ed76504c8d51a3
jdestradap <jdestradap@gmail.com>
2016-02-23 13:32:04 -0500

    reverse proxy

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reverseProxyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reverseProxyApp
+ */
+angular.module('reverseProxyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
